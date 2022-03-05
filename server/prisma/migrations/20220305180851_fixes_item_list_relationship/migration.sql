85441c2677a0ef0567a5a1f2131f1c074e3eb75d
Bruno Silva <bwmsites@gmail.com>
2022-03-05 15:34:34 -0300

    fixes prisma models relationship

---
+++ server/prisma/migrations/20220305180851_fixes_item_list_relationship/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `shoppingListItemsId` on the `item` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "item" DROP COLUMN "shoppingListItemsId";
